93fd2e764414e0afd654a332b52eb51967de51fa
Edgar Simo-Serra <bobbens@gmail.com>
2012-03-23 12:25:38 +0100

    * Handle two output parameters to generate vertex and face lists.

--- bttc_m.c
+++ bttc_m.c
@@ -1,4 +1,6 @@
-
+/*
+ * See licensing information in bttc.h
+ */
 
 #include "bttc.h"
 
@@ -15,6 +17,8 @@ void mexFunction( int nlhs, mxArray *plhs[],
    /* Check parameters. */
    if (nrhs < 2)
       mexErrMsgTxt("At least two input arguments required.");
+   if ((nlhs < 1) || (nlhs > 2))
+      mexErrMsgTxt("Function must have one or two outputs.");
    if (!mxIsDouble(prhs[0]))
       mexErrMsgTxt("Image parameter must be double matrix." );
    if (!mxIsDouble(prhs[1]))
@@ -34,18 +38,81 @@ void mexFunction( int nlhs, mxArray *plhs[],
 
    /* Actual algorithm. */
    faces = bttc( &n, img, size, size, threshold );
-   if (faces == NULL)
+   if (faces == NULL) {
       mexErrMsgTxt("bttc failed to run.");
+      return;
+   }
 
-   int dims[2];
-   dims[0] = n;
-   dims[1] = 6;
-   plhs[0] = mxCreateNumericArray( 2, dims, mxDOUBLE_CLASS, mxREAL );
-   double *dat = (double*)mxGetData(plhs[0]);
-   int i, j;
-   for (i=0; i<n; i++)
-      for (j=0; j<6; j++)
-         dat[ j*n + i ] = (double) faces[ 6*i + j ];
+   if (nlhs == 1) {
+      int dims[2];
+      dims[0] = n;
+      dims[1] = 6;
+      plhs[0] = mxCreateNumericArray( 2, dims, mxDOUBLE_CLASS, mxREAL );
+      double *dat = (double*)mxGetData(plhs[0]);
+      int i, j;
+      for (i=0; i<n; i++)
+         for (j=0; j<6; j++)
+            dat[ j*n + i ] = (double) faces[ 6*i + j ];
+   }
+   else {
+      int nvert;
+      double *vert;
+      int *face, *v;
+      int i, j, k, f;
+
+      /* Do face meshing. */
+      nvert = 0;
+      vert  = malloc( n * 3*3*sizeof(double) ); /* Worst case, every vertex is unique. */
+      face  = malloc( n * 3*sizeof(int) );
+      for (i=0; i<n; i++) {
+         for (j=0; j<3; j++) {
+            v = &faces[ 6*i + 2*j ];
+            f = 0;
+            for (k=0; k<nvert; k++) {
+               if ((v[0]==(int)vert[3*k+0]) &&
+                   (v[1]==(int)vert[3*k+1])) {
+                  f = 1;
+                  break;
+               }
+            }
+            /* Already exists. */
+            if (!f) {
+               vert[ 3*nvert+0 ] = v[0];
+               vert[ 3*nvert+1 ] = v[1];
+               vert[ 3*nvert+2 ] = img[ size*v[1] + v[0] ];
+               k      = nvert;
+               nvert++;
+            }
+
+            /* Set the face id. */
+            face[ 3*i+j ] = k;
+         }
+      }
+
+      double *dat;
+      int dims[2];
+      /* Save the vertex array. */
+      dims[0] = nvert;
+      dims[1] = 3;
+      plhs[0] = mxCreateNumericArray( 2, dims, mxDOUBLE_CLASS, mxREAL );
+      dat = (double*)mxGetData(plhs[0]);
+      for (i=0; i<nvert; i++)
+         for (j=0; j<3; j++)
+            dat[ j*dims[0] + i ] = vert[ 3*i + j ]+1.;
+      /* Save the faces. */
+      dims[0] = n;
+      plhs[1] = mxCreateNumericArray( 2, dims, mxDOUBLE_CLASS, mxREAL );
+      dat = (double*)mxGetData(plhs[1]);
+      for (i=0; i<n; i++)
+         for (j=0; j<3; j++)
+            dat[ j*dims[0] + i ] = (double)face[ 3*i + j ]+1;
+
+      /* Clean up. */
+      free(face);
+      free(vert);
+   }
+   
+   /* Clean up. */
    free(faces);
 }
 
